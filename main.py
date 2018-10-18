afb32621cea34c28bf0d97449ac1733e6ffab486
deepklim <38829219+deepklim@users.noreply.github.com>
2018-10-18 11:15:28 -0400

    Rough outline of inst-led form

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 # TODO
 # Add requirements.txt
 # Add French
-# Include MySQL db on GitHub?
 # Speed:
 	# Code in vanilla JS to avoid importing jQuery
 	# If statement to only scripts when needed
 
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-from flask_mysqldb import MySQL
 import my_forms
 
 app = Flask(__name__)
-mysql = MySQL(app)
-app.config.from_pyfile('config.cfg')
+app.config['DEBUG'] = True
 
 
 @app.route('/')
@@ -32,24 +29,14 @@ def departmental():
 
 @app.route('/instructor-led', methods=['GET', 'POST'])
 def instructor_led():
-	# Get list of courses currently in LSR
-	query = "SELECT DISTINCT course_title FROM lsr ORDER BY 1 ASC;"
-	cur = mysql.connection.cursor()
-	with mysql.connection.cursor() as cur:
-		x = cur.execute(query)
-		y = cur.fetchall()
-	#course_list = [val for tup in y for val in tup]
-	# Use list to populate dropdown menu
-	# form = my_forms.wtforms_dropdown(field_name='Course Code', my_list=course_list)
-	form = my_forms.SkillsForm(request.form)
+	form = my_forms.CourseForm(request.form)
+	if request.method == 'POST' and form.validate():
+		course_title = form.course_title.data
+		return '<h1>' + course_title + '</h1>'
 	
-	return render_template('form.html', form=form, title="Sean", button_val="Go")
-	
-	
-	
-	
-	
-	
+	return render_template('form.html', form=form, title="Login", button_val="Login")
+
+
 @app.route('/online')
 def online():
 	return render_template('online.html')
