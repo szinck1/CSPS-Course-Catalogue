544c091ee218a13cae7bd567c4713e5492d8b997
deepklim <38829219+deepklim@users.noreply.github.com>
2018-10-18 01:25:27 -0400

    Rough outline of inst-led form

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 
 from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_mysqldb import MySQL
+import my_forms
 
 app = Flask(__name__)
 mysql = MySQL(app)
@@ -29,11 +30,26 @@ def departmental():
 	return render_template('departmental.html')
 
 
-@app.route('/instructor-led')
+@app.route('/instructor-led', methods=['GET', 'POST'])
 def instructor_led():
-	return render_template('instructor-led.html')
-
-
+	# Get list of courses currently in LSR
+	query = "SELECT DISTINCT course_title FROM lsr ORDER BY 1 ASC;"
+	cur = mysql.connection.cursor()
+	with mysql.connection.cursor() as cur:
+		x = cur.execute(query)
+		y = cur.fetchall()
+	#course_list = [val for tup in y for val in tup]
+	# Use list to populate dropdown menu
+	# form = my_forms.wtforms_dropdown(field_name='Course Code', my_list=course_list)
+	form = my_forms.SkillsForm(request.form)
+	
+	return render_template('form.html', form=form, title="Sean", button_val="Go")
+	
+	
+	
+	
+	
+	
 @app.route('/online')
 def online():
 	return render_template('online.html')
--- my_forms.py
+++ my_forms.py
@@ -1,4 +1,20 @@
+import pandas as pd
 from wtforms import Form, SelectField
 
-class ChoiceForm(Form, field_name, choices):
-	choice = SelectField(field_name, choices=choices)
+# Load department list; SelectField only accepts tuples of (value, label)
+path = './data/dept_list.csv'
+dept_list = pd.read_csv(path, sep=',', header=None, squeeze=True)
+dept_list = [(dept_name, dept_name) for dept_name in dept_list]
+
+class SkillsForm(Form):
+	dept_name = SelectField('Department', choices=dept_list)
+
+cnx = mysql.connector.connect(user='Bob', password='foobar', host='127.0.0.1', database='passwords')
+query = ("SELECT * FROM pwds")
+cursor = cnx.cursor(dictionary=True)
+cursor.execute(query)
+
+
+import mysql.connector
+
+# Context manager, store password in config
