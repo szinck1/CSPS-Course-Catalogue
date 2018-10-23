d695edd5e90a727b7b66358240fcffa376727935
deepklim <38829219+deepklim@users.noreply.github.com>
2018-10-23 16:27:47 -0400

    Implement first two charts

--- highcharts/inst_led.py
+++ highcharts/inst_led.py
@@ -9,10 +9,10 @@ parser.read('config.cfg')
 
 def top_5_depts(course_title):
 	query = """
-			SELECT dept_name, COUNT(dept_name)
+			SELECT billing_dept_name, COUNT(billing_dept_name)
 			FROM lsr
 			WHERE course_title = '{0}' AND reg_status = 'Confirmed' AND no_show = 0
-			GROUP BY dept_name
+			GROUP BY billing_dept_name
 			ORDER BY 2 DESC
 			LIMIT 5;
 			""".format(course_title)
@@ -26,11 +26,12 @@ def top_5_depts(course_title):
 	results = cursor.fetchall()
 	cnx.close()
 	# Process 'results' into format required by Highcharts
-	result_processed = []
-	for tup in results:
-		key, val = tup
-		result_processed.append({'name': key, 'data': [val]})
-	return json.dumps(result_processed)
+	#result_processed = []
+	#for tup in results:
+	#	key, val = tup
+	#	result_processed.append({'name': key, 'data': [val]})
+	#return json.dumps(result_processed)
+	return results
 
 
 def top_5_classifs(course_title):
@@ -51,9 +52,36 @@ def top_5_classifs(course_title):
 	cursor.execute(query)
 	results = cursor.fetchall()
 	cnx.close()
+	return results
+
+
+def offerings_per_region(course_title):
+	query = """
+			SELECT offering_region, COUNT(offering_region)
+			FROM lsr
+			WHERE course_title = '{0}' AND reg_status = 'Confirmed' AND no_show = 0
+			GROUP BY offering_region
+			ORDER BY 2 DESC;
+			""".format(course_title)
+	# Query data from MySQL
+	cnx = mysql.connector.connect(user=parser.get('db', 'user'),
+								  password=parser.get('db', 'password'),
+								  host=parser.get('db', 'host'),
+								  database=parser.get('db', 'database'))
+	cursor = cnx.cursor()
+	cursor.execute(query)
+	results = cursor.fetchall()
+	cnx.close()
 	# Process 'results' into format required by Highcharts
-	result_processed = []
-	for tup in results:
-		key, val = tup
-		result_processed.append({'name': key, 'data': [val]})
-	return json.dumps(result_processed)
+	#result_processed = []
+	#for tup in results:
+	#	key, val = tup
+	#	result_processed.append({'name': key, 'data': [val]})
+	#return json.dumps(result_processed)
+	return results
+
+
+
+# return a dict, get(region, 0)
+
+
