050f0e1ebfb3b339babed1fe9d19eb1842ab7c3c
deepklim <38829219+deepklim@users.noreply.github.com>
2018-10-23 18:01:06 -0400

    Refactor, use internal func to query MySQL

--- my_forms.py
+++ my_forms.py
@@ -6,16 +6,22 @@ from configparser import ConfigParser
 parser = ConfigParser()
 parser.read('config.cfg')
 
-# Get distinct course titles currently in dataset
-query = ("SELECT DISTINCT course_title FROM lsr ORDER BY 1 ASC;")
-cnx = mysql.connector.connect(user=parser.get('db', 'user'),
-							  password=parser.get('db', 'password'),
-							  host=parser.get('db', 'host'),
-							  database=parser.get('db', 'database'))
-cursor = cnx.cursor()
-cursor.execute(query)
-results = cursor.fetchall()
-cnx.close()
+
+# Internal function to query data from MySQL
+def _query_mysql(query, all=True):
+	cnx = mysql.connector.connect(user=parser.get('db', 'user'),
+								  password=parser.get('db', 'password'),
+								  host=parser.get('db', 'host'),
+								  database=parser.get('db', 'database'))
+	cursor = cnx.cursor()
+	cursor.execute(query)
+	results = cursor.fetchall() if all else cursor.fetchone()
+	cnx.close()
+	return results
+
+
+query = ("SELECT DISTINCT course_title FROM lsr2018 ORDER BY 1 ASC;")
+results = _query_mysql(query)
 # SelectField takes a list of tuples
 course_titles = [(val, val) for tup in results for val in tup]
 
