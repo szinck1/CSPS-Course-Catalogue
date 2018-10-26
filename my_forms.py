c863661ca464fd855bb4cd523bedbb0305d0f13e
deepklim <38829219+deepklim@users.noreply.github.com>
2018-10-26 15:33:24 -0400

    Import query_mysql function to avoid code duplication

--- my_forms.py
+++ my_forms.py
@@ -1,27 +1,9 @@
-import mysql.connector
 from wtforms import Form, SelectField
-from configparser import ConfigParser
+from highcharts.inst_led import query_mysql
+from main_config import THIS_YEAR
 
-# Get config vals for MySQL
-parser = ConfigParser()
-parser.read('./mysql_info/mysql_config.cfg')
-
-
-# Internal function to query data from MySQL
-def _query_mysql(query, all=True):
-	cnx = mysql.connector.connect(user=parser.get('db', 'user'),
-								  password=parser.get('db', 'password'),
-								  host=parser.get('db', 'host'),
-								  database=parser.get('db', 'database'))
-	cursor = cnx.cursor()
-	cursor.execute(query)
-	results = cursor.fetchall() if all else cursor.fetchone()
-	cnx.close()
-	return results
-
-
-query = ("SELECT DISTINCT course_title FROM lsr2018 ORDER BY 1 ASC;")
-results = _query_mysql(query)
+query = "SELECT DISTINCT course_title FROM lsr{0} ORDER BY 1 ASC;".format(THIS_YEAR)
+results = query_mysql(query)
 # SelectField takes a list of tuples
 course_titles = [(val, val) for tup in results for val in tup]
 
