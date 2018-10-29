27b4fbbeef70801bc382d85d935b9a288f184666
deepklim <38829219+deepklim@users.noreply.github.com>
2018-10-29 17:52:46 -0400

    Import THIS_YEAR directly from config.py

--- dashboards_app/dashboard_routes/forms.py
+++ dashboards_app/dashboard_routes/forms.py
@@ -1,7 +1,8 @@
-from dashboards_app.dashboard_routes.inst_led import query_mysql
+from dashboards_app.config import Debug
+from dashboards_app.dashboard_routes.utils import query_mysql
 from wtforms import Form, SelectField
 
-query = "SELECT DISTINCT course_title FROM lsr{0} ORDER BY 1 ASC;".format('2018_19')
+query = "SELECT DISTINCT course_title FROM lsr{0} ORDER BY 1 ASC;".format(Debug.THIS_YEAR)
 results = query_mysql(query)
 # SelectField takes a list of tuples
 course_titles = [(val, val) for tup in results for val in tup]
