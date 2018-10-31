d61e9208141a49f43fe067889cd86c58181007e7
deepklim <38829219+deepklim@users.noreply.github.com>
2018-10-31 19:24:22 -0400

    Use regex to seperate and display course code in front

--- dashboards_app/dashboard_routes/forms.py
+++ dashboards_app/dashboard_routes/forms.py
@@ -1,19 +1,30 @@
+import re
 from dashboards_app.config import Debug
 from dashboards_app.dashboard_routes.utils import query_mysql
 from wtforms import Form, SelectField
 
 
+# Internal func to bring course codes to front of course titles
+regex = re.compile(pattern=r'[a-zA-Z]{1}\d{3}')
+def _clean_title(course_title):
+    course_code = regex.search(course_title).group()
+    course_title = regex.sub('', course_title).replace(' ()', '')
+    return '{0}: {1}'.format(course_code, course_title)
+
+
 # Build form for Instructor-Led courses
-inst_led_query = """
-				 SELECT DISTINCT course_title
-				 FROM lsr{0}
-				 ORDER BY 1 ASC;
-				 """.format(Debug.THIS_YEAR)
-inst_led_results = query_mysql(inst_led_query)
-
-# SelectField takes a list of tuples
-inst_led_titles = [(val, val) for tup in inst_led_results for val in tup]
-
-
-class InstLedForm(Form):
-	course_title = SelectField('Course Title', choices=inst_led_titles)
+def inst_led_form(field_title, lang='en'):
+	query = """
+			SELECT DISTINCT course_code, course_title_{0}
+			FROM lsr{1}
+			ORDER BY 1 ASC;
+			""".format(lang, Debug.THIS_YEAR)
+	results = query_mysql(query)
+	
+	# SelectField takes list of tuples (pass_value, display_value)
+	form_list = [(tup[0], _clean_title(tup[1])) for tup in results]
+	
+	class InstLedForm(Form):
+		course_code = SelectField(field_title, choices=form_list)
+	
+	return InstLedForm
