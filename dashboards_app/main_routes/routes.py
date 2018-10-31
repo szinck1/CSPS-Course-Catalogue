c46b9020e7b5ddfc00cf6c9452566c0c42a33b1b
deepklim <38829219+deepklim@users.noreply.github.com>
2018-10-30 22:57:55 -0400

    Quick experiment for bilingual urls

--- dashboards_app/main_routes/routes.py
+++ dashboards_app/main_routes/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, session
+from flask_babel import gettext
 
 main = Blueprint('main', __name__)
 
@@ -11,3 +12,11 @@ def index():
 @main.route('/about')
 def about():
 	return render_template('about.html')
+
+
+# Possbile way to have /en/url and /fr/url and have bilingual urls
+@main.route('/<lang>/')
+def language(lang):
+	session['lang'] = lang
+	mars = gettext('Hello')
+	return render_template('index.html')
