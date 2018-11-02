6b793386e927759e40c40d1a9e640e1e8143d408
deepklim <38829219+deepklim@users.noreply.github.com>
2018-11-01 22:51:07 -0400

    Add French: Course Info

--- dashboards_app/dashboard_routes/inst_led_queries.py
+++ dashboards_app/dashboard_routes/inst_led_queries.py
@@ -5,37 +5,37 @@ from dashboards_app.dashboard_routes.utils import query_mysql, decimal_to_float,
 
 def course_title(lang, fiscal_year, course_code):
 	query = "SELECT course_title_{0} FROM lsr{1} WHERE course_code = '{2}' LIMIT 1;".format(lang, fiscal_year, course_code)
-	course_title = query_mysql(query, all=True)
+	course_title = query_mysql(query)
 	return course_title[0][0]
 
 
 def general_info(lang, fiscal_year, course_code):
 	query_duration = "SELECT training_hours FROM lsr{0} WHERE course_code = '{1}' LIMIT 1;".format(fiscal_year, course_code)
-	duration = query_mysql(query_duration, all=True)
+	duration = query_mysql(query_duration)
 	
 	query_stream = "SELECT stream_{0} FROM lsr{1} WHERE course_code = '{2}' LIMIT 1;".format(lang, fiscal_year, course_code)
-	stream = query_mysql(query_stream, all=True)
+	stream = query_mysql(query_stream)
 	
 	query_topic = "SELECT topic_{0} FROM lsr{1} WHERE course_code = '{2}' LIMIT 1;".format(lang, fiscal_year, course_code)
-	topic = query_mysql(query_topic, all=True)
+	topic = query_mysql(query_topic)
 	
 	query_open = "SELECT COUNT(DISTINCT offering_id) FROM lsr{0} WHERE course_code = '{1}' AND offering_status = 'Open - Normal';".format(fiscal_year, course_code)
-	open = query_mysql(query_open, all=True)
+	open = query_mysql(query_open)
 	
 	query_delivered = "SELECT COUNT(DISTINCT offering_id) FROM lsr{0} WHERE course_code = '{1}' AND offering_status = 'Delivered - Normal';".format(fiscal_year, course_code)
-	delivered = query_mysql(query_delivered, all=True)
+	delivered = query_mysql(query_delivered)
 	
 	query_cancelled = "SELECT COUNT(DISTINCT offering_id) FROM lsr{0} WHERE course_code = '{1}' AND offering_status = 'Cancelled - Normal';".format(fiscal_year, course_code)
-	cancelled = query_mysql(query_cancelled, all=True)
+	cancelled = query_mysql(query_cancelled)
 	
 	query_client_reqs = "SELECT COUNT(DISTINCT offering_id) FROM lsr{0} WHERE course_code = '{1}' AND client != '';".format(fiscal_year, course_code)
-	client_reqs = query_mysql(query_client_reqs, all=True)
+	client_reqs = query_mysql(query_client_reqs)
 	
 	query_regs = "SELECT COUNT(reg_num) FROM lsr{0} WHERE course_code = '{1}' AND reg_status = 'Confirmed';".format(fiscal_year, course_code)
-	regs = query_mysql(query_regs, all=True)
+	regs = query_mysql(query_regs)
 	
 	query_no_shows = "SELECT SUM(no_show) FROM lsr{0} WHERE course_code = '{1}';".format(fiscal_year, course_code)
-	no_shows = query_mysql(query_no_shows, all=True)
+	no_shows = query_mysql(query_no_shows)
 	
 	results = [(gettext('Duration'), duration[0][0]),
 			   (gettext('Stream'), stream[0][0]),
@@ -49,39 +49,42 @@ def general_info(lang, fiscal_year, course_code):
 	return results
 
 
-def offerings_per_region(fiscal_year, course_code):
+def offerings_per_region(lang, fiscal_year, course_code):
 	query = """
-			SELECT offering_region, COUNT(DISTINCT offering_id)
-			FROM lsr{0}
-			WHERE course_code = '{1}' AND offering_status IN ('Open - Normal', 'Delivered - Normal')
-			GROUP BY offering_region;
-			""".format(fiscal_year, course_code)
-	results = query_mysql(query, all=True)
+			SELECT offering_region_{0}, COUNT(DISTINCT offering_id)
+			FROM lsr{1}
+			WHERE course_code = '{2}' AND offering_status IN ('Open - Normal', 'Delivered - Normal')
+			GROUP BY offering_region_{0};
+			""".format(lang, fiscal_year, course_code)
+	results = query_mysql(query)
 	
 	# Process results into format required by Highcharts
 	results = dict(results)
 	results_processed = []
-	all_regions = ['Atlantic', 'Ontario', 'NCR', 'Pacific', 'Prairie', 'Québec']
+	if lang == 'fr':
+		all_regions = ['Atlantique', 'Ontario', 'RCN', 'Pacifique', 'Prairie', 'Québec', 'Hors du Canada']
+	else:
+		all_regions = ['Atlantic', 'Ontario', 'NCR', 'Pacific', 'Prairie', 'Quebec', 'Outside Canada']
 	for region in all_regions:
 		count = results.get(region, 0)
 		results_processed.append({'name': region, 'data': [count]})
 	return json.dumps(results_processed)
 
 
-def offerings_per_lang(fiscal_year, course_code):
+def offerings_per_lang(lang, fiscal_year, course_code):
 	query = """
-			SELECT offering_language, COUNT(DISTINCT offering_id)
-			FROM lsr{0}
-			WHERE course_code = '{1}' AND offering_status IN ('Open - Normal', 'Delivered - Normal')
-			GROUP BY offering_language;
-			""".format(fiscal_year, course_code)
-	results = query_mysql(query, all=True)
+			SELECT offering_language_{0}, COUNT(DISTINCT offering_id)
+			FROM lsr{1}
+			WHERE course_code = '{2}' AND offering_status IN ('Open - Normal', 'Delivered - Normal')
+			GROUP BY offering_language_{0};
+			""".format(lang, fiscal_year, course_code)
+	results = query_mysql(query)
 	
 	# Process results into format required by Highcharts
 	results = dict(results)
 	results_processed = []
 	for key, val in results.items():
-		results_processed.append({'name': key.split('/')[0], 'data': [val]})
+		results_processed.append({'name': key, 'data': [val]})
 	# Account for 0 offerings
 	if not results_processed:
 		results_processed = [{'name': 'English', 'data': [0]}, {'name': 'French', 'data': [0]}]
@@ -100,32 +103,32 @@ def offerings_cancelled(fiscal_year, course_code):
 				 FROM lsr{0}
 				 WHERE course_code LIKE '{1}') AS b;
 			""".format(fiscal_year, course_code)
-	results = query_mysql(query, all=True)
+	results = query_mysql(query)
 	return decimal_to_float(results)
 
 
-def top_5_depts(fiscal_year, course_code):
+def top_5_depts(lang, fiscal_year, course_code):
 	query = """
-			SELECT billing_dept_name, COUNT(billing_dept_name)
-			FROM lsr{0}
-			WHERE course_code = '{1}' AND reg_status = 'Confirmed' AND no_show = 0
-			GROUP BY billing_dept_name
+			SELECT billing_dept_name_{0}, COUNT(billing_dept_name_{0})
+			FROM lsr{1}
+			WHERE course_code = '{2}' AND reg_status = 'Confirmed'
+			GROUP BY billing_dept_name_{0}
 			ORDER BY 2 DESC
 			LIMIT 5;
-			""".format(fiscal_year, course_code)
-	return query_mysql(query, all=True)
+			""".format(lang, fiscal_year, course_code)
+	return query_mysql(query)
 
 
 def top_5_classifs(fiscal_year, course_code):
 	query = """
 			SELECT learner_classif, COUNT(learner_classif)
 			FROM lsr{0}
-			WHERE course_code = '{1}' AND reg_status = 'Confirmed' AND no_show = 0
+			WHERE course_code = '{1}' AND reg_status = 'Confirmed'
 			GROUP BY learner_classif
 			ORDER BY 2 DESC
 			LIMIT 5;
 			""".format(fiscal_year, course_code)
-	return query_mysql(query, all=True)
+	return query_mysql(query)
 
 
 def avg_class_size(fiscal_year, course_code):
@@ -139,7 +142,7 @@ def avg_class_size(fiscal_year, course_code):
 				ORDER BY 1 DESC
 			) AS sub_table;
 			""".format(fiscal_year, course_code)
-	results = query_mysql(query, all=True)
+	results = query_mysql(query)
 	return decimal_to_int(results)
 
 
@@ -155,5 +158,5 @@ def avg_no_shows(fiscal_year, course_code):
 				 FROM lsr{0}
 				 WHERE course_code LIKE '{1}' AND offering_status IN ('Open - Normal', 'Delivered - Normal')) AS b;
 			""".format(fiscal_year, course_code)
-	results = query_mysql(query, all=True)
+	results = query_mysql(query)
 	return decimal_to_float(results)
--- dashboards_app/dashboard_routes/routes.py
+++ dashboards_app/dashboard_routes/routes.py
@@ -19,7 +19,7 @@ def context_processor():
 def instructor_led():
 	field_title = gettext('Course Title')
 	lang = session.get('lang', 'en')
-	form = inst_led_form(field_title, lang)
+	form = inst_led_form(lang, field_title)
 	form = form(request.form)
 	
 	if request.method == 'POST' and form.validate():
@@ -39,25 +39,26 @@ def instructor_led_dash():
 	
 	# Run queries and save in dict to be passed to templates
 	pass_dict = {
+		'course_code': course_code,
         'course_title': inst_led_queries.course_title(lang, THIS_YEAR, course_code),
 		'general_info_LY': inst_led_queries.general_info(lang, LAST_YEAR, course_code),
-		'general_info_TY': inst_led_queries.general_info(lang, THIS_YEAR, course_code)
-		# 'offerings_per_region': inst_led_queries.offerings_per_region(THIS_YEAR, course_title),
-		# 'offerings_per_lang': inst_led_queries.offerings_per_lang(THIS_YEAR, course_title),
-		# 'offerings_cancelled_overall_LY': inst_led_queries.offerings_cancelled(LAST_YEAR, '%'),
-		# 'offerings_cancelled_overall_TY': inst_led_queries.offerings_cancelled(THIS_YEAR, '%'),
-		# 'offerings_cancelled_LY': inst_led_queries.offerings_cancelled(LAST_YEAR, course_title),
-		# 'offerings_cancelled_TY': inst_led_queries.offerings_cancelled(THIS_YEAR, course_title),
-		# 'top_5_depts': inst_led_queries.top_5_depts(THIS_YEAR, course_title),
-		# 'top_5_classifs': inst_led_queries.top_5_classifs(THIS_YEAR, course_title),
-		# 'avg_class_size_overall_LY': inst_led_queries.avg_class_size(LAST_YEAR, '%'),
-		# 'avg_class_size_overall_TY': inst_led_queries.avg_class_size(THIS_YEAR, '%'),
-		# 'avg_class_size_LY': inst_led_queries.avg_class_size(LAST_YEAR, course_title),
-		# 'avg_class_size_TY': inst_led_queries.avg_class_size(THIS_YEAR, course_title),
-		# 'avg_no_shows_overall_LY': round(inst_led_queries.avg_no_shows(LAST_YEAR, '%'), 1),
-		# 'avg_no_shows_overall_TY': round(inst_led_queries.avg_no_shows(THIS_YEAR, '%'), 1),
-		# 'avg_no_shows_LY': round(inst_led_queries.avg_no_shows(LAST_YEAR, course_title), 1),
-		# 'avg_no_shows_TY': round(inst_led_queries.avg_no_shows(THIS_YEAR, course_title), 1)
+		'general_info_TY': inst_led_queries.general_info(lang, THIS_YEAR, course_code),
+		'offerings_per_region': inst_led_queries.offerings_per_region(lang, THIS_YEAR, course_code),
+		'offerings_per_lang': inst_led_queries.offerings_per_lang(lang, THIS_YEAR, course_code),
+		'offerings_cancelled_overall_LY': inst_led_queries.offerings_cancelled(LAST_YEAR, '%'),
+		'offerings_cancelled_overall_TY': inst_led_queries.offerings_cancelled(THIS_YEAR, '%'),
+		'offerings_cancelled_LY': inst_led_queries.offerings_cancelled(LAST_YEAR, course_code),
+		'offerings_cancelled_TY': inst_led_queries.offerings_cancelled(THIS_YEAR, course_code),
+		'top_5_depts': inst_led_queries.top_5_depts(lang, THIS_YEAR, course_code),
+		'top_5_classifs': inst_led_queries.top_5_classifs(THIS_YEAR, course_code),
+		'avg_class_size_overall_LY': inst_led_queries.avg_class_size(LAST_YEAR, '%'),
+		'avg_class_size_overall_TY': inst_led_queries.avg_class_size(THIS_YEAR, '%'),
+		'avg_class_size_LY': inst_led_queries.avg_class_size(LAST_YEAR, course_code),
+		'avg_class_size_TY': inst_led_queries.avg_class_size(THIS_YEAR, course_code),
+		'avg_no_shows_overall_LY': round(inst_led_queries.avg_no_shows(LAST_YEAR, '%'), 1),
+		'avg_no_shows_overall_TY': round(inst_led_queries.avg_no_shows(THIS_YEAR, '%'), 1),
+		'avg_no_shows_LY': round(inst_led_queries.avg_no_shows(LAST_YEAR, course_code), 1),
+		'avg_no_shows_TY': round(inst_led_queries.avg_no_shows(THIS_YEAR, course_code), 1)
 	}
 	return render_template('instructor-led.html', pass_dict=pass_dict)
 
