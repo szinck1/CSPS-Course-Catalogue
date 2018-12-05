acaaa2ea7a3f307be046503318a04b9a918c6b34
deepklim <38829219+deepklim@users.noreply.github.com>
2018-12-05 16:57:51 -0500

    Extract all questions with answers, not just main 4

--- catalogue_app/course_routes/queries/ratings_queries.py
+++ catalogue_app/course_routes/queries/ratings_queries.py
@@ -2,24 +2,44 @@ from flask_babel import gettext
 from catalogue_app.course_routes.utils import query_mysql
 
 
-def drf_average(fiscal_year, short_question, course_code):
+def all_ratings(fiscal_year, course_code):
 	table_name = 'ratings{0}'.format(fiscal_year)
-	query = """
-	SELECT month, AVG(numerical_answer)
-	FROM {0}
-	WHERE course_code = %s AND short_question = %s
-	GROUP BY month
-	ORDER BY month_num;
+	# Get list of questions answered for given course code
+	questions_query = """
+		SELECT DISTINCT short_question
+		FROM {0}
+		WHERE course_code = %s
+		ORDER BY 1 ASC;
 	""".format(table_name)
-	results = query_mysql(query, (course_code, short_question))
+	questions = query_mysql(questions_query, (course_code,))
+	questions = [tup[0] for tup in questions]
+	# Account courses with no feedback
+	if not questions:
+		return False
 	
-	# Process results into format required by Highcharts
+	# Query each question for monthly results
+	query = """
+		SELECT month, numerical_answer
+		FROM {0}
+		WHERE course_code = %s AND short_question = %s;
+	""".format(table_name)
+	# Return a list of dictionaries
+	return_list = []
+	for question in questions:
+		results = query_mysql(query, (course_code, question))
+		results = dict(results)
+		results_processed = _add_months(results)
+		# Use str.title() method to nicely format question
+		return_list.append({question.title(): results_processed})
+	return return_list
+
+
+# Helper function to ensure every month accounted for
+def _add_months(my_dict):
 	months = ['April', 'May', 'June', 'July', 'August', 'September', 'October',
 			  'November', 'December', 'January', 'February', 'March']
-	results = dict(results)
-	results_processed = []
+	return_list = []
 	for month in months:
-		count = results.get(month, 0)
-		count = round(float(count), 2)
-		results_processed.append(count)
-	return results_processed
+		count = my_dict.get(month, 0)
+		return_list.append(count)
+	return return_list
