5ed16666954bbfe5a063a0b112183608a7e3c227
deepklim <38829219+deepklim@users.noreply.github.com>
2018-12-07 13:50:16 -0500

    Refactor into general function for all categorical questions

--- catalogue_app/course_routes/queries/comments_queries.py
+++ catalogue_app/course_routes/queries/comments_queries.py
@@ -15,31 +15,14 @@ def fetch_comments(question, course_code):
 	return results
 
 
-def reason_to_participate(course_code):
+def fetch_categorical(course_code, question):
 	query = """
 	SELECT text_answer, COUNT(text_answer)
 	FROM comments
-	WHERE course_code = %s AND short_question = 'Reason to Participate'
+	WHERE course_code = %s AND short_question = '{0}'
 	GROUP BY text_answer
 	ORDER BY 1 ASC;
-	"""
-	results = query_mysql(query, (course_code,))
-	
-	# Process results into format required by Highcharts
-	results_processed = []
-	for tup in results:
-		results_processed.append({'name': tup[0], 'y': tup[1]})
-	return results_processed if results_processed else [{'name': 'No response', 'y': 1}]
-
-
-def technical_issues(course_code):
-	query = """
-	SELECT text_answer, COUNT(text_answer)
-	FROM comments
-	WHERE course_code = %s AND short_question IN ('Technical Issues', 'Technical Issues?')
-	GROUP BY text_answer
-	ORDER BY 1 ASC;
-	"""
+	""".format(question)
 	results = query_mysql(query, (course_code,))
 	
 	# Process results into format required by Highcharts
