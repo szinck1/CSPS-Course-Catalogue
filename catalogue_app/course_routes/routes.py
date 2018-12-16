315c2467cd2ab29a1e4d53e487fd163bb239100b
deepklim <38829219+deepklim@users.noreply.github.com>
2018-12-16 15:41:38 -0500

    Split lsr_queries, too monolithic

--- catalogue_app/course_routes/routes.py
+++ catalogue_app/course_routes/routes.py
@@ -2,8 +2,12 @@ from flask import Blueprint, redirect, render_template, request, session, url_fo
 from flask_babel import gettext
 from catalogue_app.config import Debug
 from catalogue_app.course_routes.form import course_title_form, course_code_form
-from catalogue_app.course_routes.queries import lsr_queries, product_info_queries, ratings_queries, comments_queries
+from catalogue_app.course_routes.queries import comment_queries, general_queries, learner_queries, map_queries
+from catalogue_app.course_routes.queries import offering_queries, rating_queries
+
+# Instantiate blueprint
 course = Blueprint('course', __name__)
+
 
 # Make LAST_YEAR and THIS_YEAR available to all templates
 LAST_YEAR = Debug.LAST_YEAR
@@ -52,12 +56,12 @@ def course_result():
 	lang = session.get('lang', 'en')
 	
 	# If course_code doesn't exist, render not_found.html
-	course_title = lsr_queries.course_title(lang, THIS_YEAR, course_code)
+	course_title = general_queries.course_title(lang, THIS_YEAR, course_code)
 	if not course_title:
 		return render_template('not-found.html')
 	
 	# Check if course is online - needed for certain templates
-	online_course = lsr_queries.online_course(THIS_YEAR, course_code)
+	online_course = general_queries.online_course(THIS_YEAR, course_code)
 	
 	# Run queries and save in dict to be passed to templates
 	pass_dict = {
@@ -66,46 +70,46 @@ def course_result():
         'course_title': course_title,
 		'online_course': online_course,
 		# General
-		'course_description': product_info_queries.course_description(lang, course_code),
-		'course_info': product_info_queries.course_info(lang, course_code),
+		'course_description': general_queries.course_description(lang, course_code),
+		'course_info': general_queries.course_info(lang, course_code),
 		# Dashboard
-		'overall_numbers_LY': lsr_queries.overall_numbers(LAST_YEAR, course_code),
-		'overall_numbers_TY': lsr_queries.overall_numbers(THIS_YEAR, course_code),
-		'offerings_per_region': lsr_queries.offerings_per_region(THIS_YEAR, course_code),
-		'province_drilldown': lsr_queries.province_drilldown(THIS_YEAR, course_code),
-		'city_drilldown': lsr_queries.city_drilldown(THIS_YEAR, course_code),
-		'offerings_per_lang_LY': lsr_queries.offerings_per_lang(LAST_YEAR, course_code),
-		'offerings_per_lang_TY': lsr_queries.offerings_per_lang(THIS_YEAR, course_code),
-		'offerings_cancelled_global_LY': lsr_queries.offerings_cancelled_global(LAST_YEAR),
-		'offerings_cancelled_global_TY': lsr_queries.offerings_cancelled_global(THIS_YEAR),
-		'offerings_cancelled_LY': lsr_queries.offerings_cancelled(LAST_YEAR, course_code),
-		'offerings_cancelled_TY': lsr_queries.offerings_cancelled(THIS_YEAR, course_code),
-		'top_5_depts': lsr_queries.top_5_depts(lang, THIS_YEAR, course_code),
-		'top_5_classifs': lsr_queries.top_5_classifs(THIS_YEAR, course_code),
-		'avg_class_size_global_LY': lsr_queries.avg_class_size_global(LAST_YEAR),
-		'avg_class_size_global_TY': lsr_queries.avg_class_size_global(THIS_YEAR),
-		'avg_class_size_LY': lsr_queries.avg_class_size(LAST_YEAR, course_code),
-		'avg_class_size_TY': lsr_queries.avg_class_size(THIS_YEAR, course_code),
-		'avg_no_shows_global_LY': round(lsr_queries.avg_no_shows_global(LAST_YEAR), 1),
-		'avg_no_shows_global_TY': round(lsr_queries.avg_no_shows_global(THIS_YEAR), 1),
-		'avg_no_shows_LY': round(lsr_queries.avg_no_shows(LAST_YEAR, course_code), 1),
-		'avg_no_shows_TY': round(lsr_queries.avg_no_shows(THIS_YEAR, course_code), 1),
-		# Geodata
-		'offering_city_counts': lsr_queries.offering_city_counts(THIS_YEAR, course_code),
-		'learner_city_counts': lsr_queries.learner_city_counts(THIS_YEAR, course_code),
+		'overall_numbers_LY': offering_queries.overall_numbers(LAST_YEAR, course_code),
+		'overall_numbers_TY': offering_queries.overall_numbers(THIS_YEAR, course_code),
+		'offerings_per_region': offering_queries.offerings_per_region(THIS_YEAR, course_code),
+		'province_drilldown': offering_queries.province_drilldown(THIS_YEAR, course_code),
+		'city_drilldown': offering_queries.city_drilldown(THIS_YEAR, course_code),
+		'offerings_per_lang_LY': offering_queries.offerings_per_lang(LAST_YEAR, course_code),
+		'offerings_per_lang_TY': offering_queries.offerings_per_lang(THIS_YEAR, course_code),
+		'offerings_cancelled_global_LY': offering_queries.offerings_cancelled_global(LAST_YEAR),
+		'offerings_cancelled_global_TY': offering_queries.offerings_cancelled_global(THIS_YEAR),
+		'offerings_cancelled_LY': offering_queries.offerings_cancelled(LAST_YEAR, course_code),
+		'offerings_cancelled_TY': offering_queries.offerings_cancelled(THIS_YEAR, course_code),
+		'top_5_depts': learner_queries.top_5_depts(lang, THIS_YEAR, course_code),
+		'top_5_classifs': learner_queries.top_5_classifs(THIS_YEAR, course_code),
+		'avg_class_size_global_LY': offering_queries.avg_class_size_global(LAST_YEAR),
+		'avg_class_size_global_TY': offering_queries.avg_class_size_global(THIS_YEAR),
+		'avg_class_size_LY': offering_queries.avg_class_size(LAST_YEAR, course_code),
+		'avg_class_size_TY': offering_queries.avg_class_size(THIS_YEAR, course_code),
+		'avg_no_shows_global_LY': round(offering_queries.avg_no_shows_global(LAST_YEAR), 1),
+		'avg_no_shows_global_TY': round(offering_queries.avg_no_shows_global(THIS_YEAR), 1),
+		'avg_no_shows_LY': round(offering_queries.avg_no_shows(LAST_YEAR, course_code), 1),
+		'avg_no_shows_TY': round(offering_queries.avg_no_shows(THIS_YEAR, course_code), 1),
+		# Maps
+		'offering_city_counts': map_queries.offering_city_counts(THIS_YEAR, course_code),
+		'learner_city_counts': map_queries.learner_city_counts(THIS_YEAR, course_code),
 		# Ratings
-		'all_ratings': ratings_queries.all_ratings(THIS_YEAR, course_code),
+		'all_ratings': rating_queries.all_ratings(THIS_YEAR, course_code),
 		# Comments
-		'general_comments': comments_queries.fetch_comments(course_code, 'Comment - General '),
-		'technical_comments': comments_queries.fetch_comments(course_code, 'Issue Description'),
-		'language_comments': comments_queries.fetch_comments(course_code, 'Comment - OL Not Available'),
-		'performance_comments': comments_queries.fetch_comments(course_code, 'Comment - application for performance improvement'),
+		'general_comments': comment_queries.fetch_comments(course_code, 'Comment - General '),
+		'technical_comments': comment_queries.fetch_comments(course_code, 'Issue Description'),
+		'language_comments': comment_queries.fetch_comments(course_code, 'Comment - OL Not Available'),
+		'performance_comments': comment_queries.fetch_comments(course_code, 'Comment - application for performance improvement'),
 		# Categorical and yes/no questions
-		'reason_to_participate': comments_queries.fetch_categorical(course_code, 'Reason to Participate'),
-		'technical_issues': comments_queries.fetch_categorical(course_code, 'Technical Issues'),
-		'languages_available': comments_queries.fetch_categorical(course_code, 'Official Language Available '),
-		'tools_used': comments_queries.fetch_categorical(course_code, 'GCCampus Tools Used'),
-		'prepared_by': comments_queries.fetch_categorical(course_code, 'Prep')
+		'reason_to_participate': comment_queries.fetch_categorical(course_code, 'Reason to Participate'),
+		'technical_issues': comment_queries.fetch_categorical(course_code, 'Technical Issues'),
+		'languages_available': comment_queries.fetch_categorical(course_code, 'Official Language Available '),
+		'tools_used': comment_queries.fetch_categorical(course_code, 'GCCampus Tools Used'),
+		'prepared_by': comment_queries.fetch_categorical(course_code, 'Prep')
 	}
 	return render_template('/course-page/main.html', pass_dict=pass_dict)
 
