ec8f16c513a20c8aa273c81a3a073233ac9654c8
deepklim <38829219+deepklim@users.noreply.github.com>
2018-12-19 12:24:25 -0500

    Add Attendance (registrations) per Month

--- catalogue_app/course_routes/queries/learner_queries.py
+++ catalogue_app/course_routes/queries/learner_queries.py
@@ -1,6 +1,27 @@
 from catalogue_app.course_routes.utils import query_mysql
 
 
+def regs_per_month(fiscal_year, course_code):
+	table_name = 'lsr{0}'.format(fiscal_year)
+	query = """
+		SELECT month, COUNT(reg_id)
+		FROM {0}
+		WHERE course_code = %s AND reg_status = 'Confirmed'
+		GROUP BY month;
+		""".format(table_name)
+	results = query_mysql(query, (course_code,))
+	results = dict(results)
+	
+	# Process results into format required by Highcharts
+	results_processed = []
+	months = ['April', 'May', 'June', 'July', 'August', 'September', 'October',
+			  'November', 'December', 'January', 'February', 'March']
+	for month in months:
+		count = results.get(month, 0)
+		results_processed.append({'name': month, 'y': count})
+	return results_processed
+
+
 def top_5_depts(lang, fiscal_year, course_code):
 	field_name =  'billing_dept_name_{0}'.format(lang)
 	table_name = 'lsr{0}'.format(fiscal_year)
--- catalogue_app/course_routes/routes.py
+++ catalogue_app/course_routes/routes.py
@@ -72,7 +72,7 @@ def course_result():
 		# General
 		'course_description': general_queries.course_description(lang, course_code),
 		'course_info': general_queries.course_info(lang, course_code),
-		# Dashboard
+		# Dashboard - offerings
 		'overall_numbers_LY': offering_queries.overall_numbers(LAST_YEAR, course_code),
 		'overall_numbers_TY': offering_queries.overall_numbers(THIS_YEAR, course_code),
 		'offerings_per_region': offering_queries.offerings_per_region(THIS_YEAR, course_code),
@@ -84,8 +84,6 @@ def course_result():
 		'offerings_cancelled_global_TY': offering_queries.offerings_cancelled_global(THIS_YEAR),
 		'offerings_cancelled_LY': offering_queries.offerings_cancelled(LAST_YEAR, course_code),
 		'offerings_cancelled_TY': offering_queries.offerings_cancelled(THIS_YEAR, course_code),
-		'top_5_depts': learner_queries.top_5_depts(lang, THIS_YEAR, course_code),
-		'top_5_classifs': learner_queries.top_5_classifs(THIS_YEAR, course_code),
 		'avg_class_size_global_LY': offering_queries.avg_class_size_global(LAST_YEAR),
 		'avg_class_size_global_TY': offering_queries.avg_class_size_global(THIS_YEAR),
 		'avg_class_size_LY': offering_queries.avg_class_size(LAST_YEAR, course_code),
@@ -94,6 +92,10 @@ def course_result():
 		'avg_no_shows_global_TY': round(offering_queries.avg_no_shows_global(THIS_YEAR), 1),
 		'avg_no_shows_LY': round(offering_queries.avg_no_shows(LAST_YEAR, course_code), 1),
 		'avg_no_shows_TY': round(offering_queries.avg_no_shows(THIS_YEAR, course_code), 1),
+		# Dashboard - learners
+		'regs_per_month': learner_queries.regs_per_month(THIS_YEAR, course_code),
+		'top_5_depts': learner_queries.top_5_depts(lang, THIS_YEAR, course_code),
+		'top_5_classifs': learner_queries.top_5_classifs(THIS_YEAR, course_code),
 		# Maps
 		'offering_city_counts': map_queries.offering_city_counts(THIS_YEAR, course_code),
 		'learner_city_counts': map_queries.learner_city_counts(THIS_YEAR, course_code),
