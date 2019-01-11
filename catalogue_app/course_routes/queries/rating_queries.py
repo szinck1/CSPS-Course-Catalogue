0e6a845897ddb96f92a428cc27a6412b0fe64a33
deepklim <38829219+deepklim@users.noreply.github.com>
2019-01-11 13:39:08 -0500

    Show questions' full text in tooltip

--- catalogue_app/course_routes/queries/rating_queries.py
+++ catalogue_app/course_routes/queries/rating_queries.py
@@ -4,15 +4,15 @@ from catalogue_app.course_routes.utils import query_mysql
 
 def all_ratings(course_code, lang):
 	# Get list of questions answered for given course code
-	field_name = 'short_question_{0}'.format(lang)
+	field_name_1 = 'short_question_{0}'.format(lang)
+	field_name_2 = 'long_question_{0}'.format(lang)
 	questions_query = """
-		SELECT DISTINCT {0}
+		SELECT DISTINCT {0}, {1}
 		FROM ratings
 		WHERE course_code = %s
 		ORDER BY 1 ASC;
-	""".format(field_name)
+	""".format(field_name_1, field_name_2)
 	questions = query_mysql(questions_query, (course_code,))
-	questions = [tup[0] for tup in questions]
 	# Account courses with no feedback
 	if not questions:
 		return False
@@ -22,16 +22,16 @@ def all_ratings(course_code, lang):
 		SELECT month, numerical_answer, count
 		FROM ratings
 		WHERE course_code = %s AND {0} = %s;
-	""".format(field_name)
+	""".format(field_name_1)
 	# Return a list of dictionaries
 	return_list = []
 	for question in questions:
-		results = query_mysql(query, (course_code, question))
-		# Convert 'results' from format [(month, numerical_answer, count), ...] to {month: (numerical_answer, count), ...}
+		results = query_mysql(query, (course_code, question[0]))
+		# Convert 'results' from format [(April, numerical_answer, count), ...] to {April: (numerical_answer, count), May: ...}
 		results = [(tup[0], {'y': tup[1], 'count': tup[2]}) for tup in results]
 		results = dict(results)
 		results_processed = _add_months(results)
-		return_list.append((question, results_processed))
+		return_list.append((question[0], question[1], results_processed))
 	return return_list
 
 
