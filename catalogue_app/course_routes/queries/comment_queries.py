a1ba7a390be6a1e84de5221e1ca5e4a954243c76
deepklim <38829219+deepklim@users.noreply.github.com>
2019-01-22 16:25:22 -0500

    Consolidate queries: Categorical questions

--- catalogue_app/course_routes/queries/comment_queries.py
+++ catalogue_app/course_routes/queries/comment_queries.py
@@ -4,9 +4,7 @@ from catalogue_app.db import query_mysql
 
 # This query probably needs an index
 # Should probably store results_processed as attribute
-
-
-class Comments2:
+class Comments:
 	def __init__(self, course_code):
 		self.course_code = course_code
 		self.comment_data = None
@@ -25,14 +23,21 @@ class Comments2:
 	
 	
 	def load(self):
-		# Query all comment and categorical questions
+		# Query all comments and categorical questions
 		self._load_all_comments()
 		self._load_all_categorical()
 		# Parse with Pandas and process into form required by Highcharts
+		# Comments
 		self.general = self._load_comment('Comment - General')
 		self.technical = self._load_comment('Comment - Technical')
 		self.language = self._load_comment('Comment - OL')
 		self.performance = self._load_comment('Comment - Performance')
+		# Categorical and true/false questions
+		self.reason = self._load_categorical('Reason to Participate')
+		self.technical_bool = self._load_categorical('Technical Issues')
+		self.language_bool = self._load_categorical('OL Available')
+		self.gccampus_bool = self._load_categorical('GCcampus Tools Used')
+		self.preparation = self._load_categorical('Prep')
 		# Return self to allow method chaining
 		return self
 	
@@ -52,6 +57,19 @@ class Comments2:
 			tup = (text_answer, stars, learner_classif, offering_city, fiscal_year, quarter)
 			results_processed.append(tup)
 		return results_processed
+	
+	
+	def _load_categorical(self, question):
+		data_filtered = self.categorical_data.loc[self.categorical_data['short_question'] == question, :]
+		results_processed = []
+		for row in data_filtered.itertuples(index=False):
+			# Unpack the tuple as some fields require customization
+			answer = row[1]
+			count = row[2]
+			# Reassemble and append
+			dict_ = {'name': answer, 'y': count}
+			results_processed.append(dict_)
+		return results_processed if results_processed else [{'name': 'No response', 'y': 1}]
 	
 	
 	def _load_all_comments(self):
@@ -74,52 +92,17 @@ class Comments2:
 	
 	
 	def _load_all_categorical(self):
-		pass
-
-
-
-
-
-
-
-
-
-
-
-
-class Comments:
-	def __init__(self, course_code):
-		self.course_code = course_code
-		# Categorical and true/false questions
-		self.reason = None
-		self.technical_bool = None
-		self.language_bool = None
-		self.gccampus_bool = None
-		self.preparation = None
-	
-	
-	def load(self):
-		# Categorical and true/false questions
-		self.reason = self._load_categorical('Reason to Participate')
-		self.technical_bool = self._load_categorical('Technical Issues')
-		self.language_bool = self._load_categorical('OL Available')
-		self.gccampus_bool = self._load_categorical('GCcampus Tools Used')
-		self.preparation = self._load_categorical('Prep')
-		# Return self to allow method chaining
-		return self
-	
-	
-	def _load_categorical(self, question):
 		query = """
-			SELECT text_answer, COUNT(text_answer)
+			SELECT short_question, text_answer, COUNT(text_answer)
 			FROM comments
-			WHERE course_code = %s AND short_question = %s
-			GROUP BY text_answer
+			WHERE
+				course_code = %s
+				AND
+				short_question IN ('Reason to Participate', 'Technical Issues', 'OL Available', 'GCcampus Tools Used', 'Prep')
+			GROUP BY short_question, text_answer
 			ORDER BY 1 ASC;
 		"""
-		results = query_mysql(query, (self.course_code, question))
-		# Process results into format required by Highcharts
-		results_processed = []
-		for tup in results:
-			results_processed.append({'name': tup[0], 'y': tup[1]})
-		return results_processed if results_processed else [{'name': 'No response', 'y': 1}]
+		results = query_mysql(query, (self.course_code,))
+		results = pd.DataFrame(results, columns=['short_question', 'text_answer', 'count'])
+		# Return False if course has received no feedback
+		self.categorical_data = False if results.empty else results
