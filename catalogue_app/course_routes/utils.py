f0edd7761abd61eda64d0338a6714c2f6ac55fa1
deepklim <38829219+deepklim@users.noreply.github.com>
2019-01-24 14:35:40 -0500

    Add docstring

--- catalogue_app/course_routes/utils.py
+++ catalogue_app/course_routes/utils.py
@@ -1,17 +1,21 @@
+# Make a class
 
-# Helper functions to convert MySQL results to desired dtype
 def as_string(my_val, error_msg=False):
+	"""Convert from MySQL to Python dtypes."""
 	# Account for MySQL returning NULL
 	if not my_val or not my_val[0][0]:
 		return error_msg
 	else:
 		return str(my_val[0][0])
 
+
 def as_float(my_val):
 	return float(as_string(my_val))
 
+
 def as_int(my_val):
 	return int(as_float(my_val))
+
 
 def as_percent(my_val):
 	return round(as_float(my_val), 2) * 100
