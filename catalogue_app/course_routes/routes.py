1b6dee712225be5a6d664dcccc2190ae31fbcfaa
deepklim <38829219+deepklim@users.noreply.github.com>
2019-01-31 11:28:08 -0500

    More elegant solution to memoize all queries to MySQL

--- catalogue_app/course_routes/routes.py
+++ catalogue_app/course_routes/routes.py
@@ -3,7 +3,7 @@ import pickle
 import time
 from flask import Blueprint, redirect, render_template, request, session, url_for
 from flask_babel import gettext
-from catalogue_app import auth, memo_dict
+from catalogue_app import auth
 from catalogue_app.config import Config
 from catalogue_app.course_routes.forms import course_form
 from catalogue_app.course_routes.queries import (
@@ -55,69 +55,59 @@ def course_result():
 	if not course_title:
 		return render_template('not-found.html')
 	
-	# Run queries and save in dict to be passed to templates
-	# Check if already memoized in app var memo_dict
-	if course_code in memo_dict:
-		print('Using memoized values')
-		return render_template('/course-page/main.html', pass_dict=copy.deepcopy(memo_dict[course_code]))
-	else:
-		print('Running query for first time')
-		
-		# Instantiate classes
-		locations = offering_queries.OfferingLocations(THIS_YEAR, course_code).load()
-		ratings = rating_queries.Ratings(course_code, lang).load()
-		comments = comment_queries.Comments(course_code).load()
-		
-		pass_dict = {
-			#Global
-			'course_code': course_code,
-			'course_title': course_title,
-			# General
-			'course_info': general_queries.course_info(course_code),
-			# Dashboard - offerings
-			'overall_numbers_LY': offering_queries.overall_numbers(LAST_YEAR, course_code),
-			'overall_numbers_TY': offering_queries.overall_numbers(THIS_YEAR, course_code),
-			'region_drilldown': locations.region_drilldown(),
-			'province_drilldown': locations.province_drilldown(),
-			'city_drilldown': locations.city_drilldown(),
-			'offerings_per_lang_LY': offering_queries.offerings_per_lang(LAST_YEAR, course_code),
-			'offerings_per_lang_TY': offering_queries.offerings_per_lang(THIS_YEAR, course_code),
-			'offerings_cancelled_global_LY': offering_queries.offerings_cancelled_global(LAST_YEAR),
-			'offerings_cancelled_global_TY': offering_queries.offerings_cancelled_global(THIS_YEAR),
-			'offerings_cancelled_LY': offering_queries.offerings_cancelled(LAST_YEAR, course_code),
-			'offerings_cancelled_TY': offering_queries.offerings_cancelled(THIS_YEAR, course_code),
-			'avg_class_size_global_LY': offering_queries.avg_class_size_global(LAST_YEAR),
-			'avg_class_size_global_TY': offering_queries.avg_class_size_global(THIS_YEAR),
-			'avg_class_size_LY': offering_queries.avg_class_size(LAST_YEAR, course_code),
-			'avg_class_size_TY': offering_queries.avg_class_size(THIS_YEAR, course_code),
-			'avg_no_shows_global_LY': round(offering_queries.avg_no_shows_global(LAST_YEAR), 1),
-			'avg_no_shows_global_TY': round(offering_queries.avg_no_shows_global(THIS_YEAR), 1),
-			'avg_no_shows_LY': round(offering_queries.avg_no_shows(LAST_YEAR, course_code), 1),
-			'avg_no_shows_TY': round(offering_queries.avg_no_shows(THIS_YEAR, course_code), 1),
-			# Dashboard - learners
-			'regs_per_month': learner_queries.regs_per_month(THIS_YEAR, course_code),
-			'top_5_depts': learner_queries.top_5_depts(lang, THIS_YEAR, course_code),
-			'top_5_classifs': learner_queries.top_5_classifs(THIS_YEAR, course_code),
-			# Maps
-			'offering_city_counts': map_queries.offering_city_counts(THIS_YEAR, course_code),
-			'learner_city_counts': map_queries.learner_city_counts(THIS_YEAR, course_code),
-			# Ratings
-			'all_ratings': ratings.all_ratings(),
-			# Comments
-			'general_comments': comments.general,
-			'technical_comments': comments.technical,
-			'language_comments': comments.language,
-			'performance_comments': comments.performance,
-			# Categorical and yes/no questions
-			'reason_to_participate': comments.reason,
-			'technical_issues': comments.technical_bool,
-			'languages_available': comments.language_bool,
-			'tools_used': comments.gccampus_bool,
-			'prepared_by': comments.preparation
-		}
-		# Memoize new query results
-		memo_dict[course_code] = pass_dict
-		return render_template('/course-page/main.html', pass_dict=copy.deepcopy(memo_dict[course_code]))
+	# Instantiate classes
+	locations = offering_queries.OfferingLocations(THIS_YEAR, course_code).load()
+	ratings = rating_queries.Ratings(course_code, lang).load()
+	comments = comment_queries.Comments(course_code).load()
+	
+	pass_dict = {
+		#Global
+		'course_code': course_code,
+		'course_title': course_title,
+		# General
+		'course_info': general_queries.course_info(course_code),
+		# Dashboard - offerings
+		'overall_numbers_LY': offering_queries.overall_numbers(LAST_YEAR, course_code),
+		'overall_numbers_TY': offering_queries.overall_numbers(THIS_YEAR, course_code),
+		'region_drilldown': locations.region_drilldown(),
+		'province_drilldown': locations.province_drilldown(),
+		'city_drilldown': locations.city_drilldown(),
+		'offerings_per_lang_LY': offering_queries.offerings_per_lang(LAST_YEAR, course_code),
+		'offerings_per_lang_TY': offering_queries.offerings_per_lang(THIS_YEAR, course_code),
+		'offerings_cancelled_global_LY': offering_queries.offerings_cancelled_global(LAST_YEAR),
+		'offerings_cancelled_global_TY': offering_queries.offerings_cancelled_global(THIS_YEAR),
+		'offerings_cancelled_LY': offering_queries.offerings_cancelled(LAST_YEAR, course_code),
+		'offerings_cancelled_TY': offering_queries.offerings_cancelled(THIS_YEAR, course_code),
+		'avg_class_size_global_LY': offering_queries.avg_class_size_global(LAST_YEAR),
+		'avg_class_size_global_TY': offering_queries.avg_class_size_global(THIS_YEAR),
+		'avg_class_size_LY': offering_queries.avg_class_size(LAST_YEAR, course_code),
+		'avg_class_size_TY': offering_queries.avg_class_size(THIS_YEAR, course_code),
+		'avg_no_shows_global_LY': round(offering_queries.avg_no_shows_global(LAST_YEAR), 1),
+		'avg_no_shows_global_TY': round(offering_queries.avg_no_shows_global(THIS_YEAR), 1),
+		'avg_no_shows_LY': round(offering_queries.avg_no_shows(LAST_YEAR, course_code), 1),
+		'avg_no_shows_TY': round(offering_queries.avg_no_shows(THIS_YEAR, course_code), 1),
+		# Dashboard - learners
+		'regs_per_month': learner_queries.regs_per_month(THIS_YEAR, course_code),
+		'top_5_depts': learner_queries.top_5_depts(lang, THIS_YEAR, course_code),
+		'top_5_classifs': learner_queries.top_5_classifs(THIS_YEAR, course_code),
+		# Maps
+		'offering_city_counts': map_queries.offering_city_counts(THIS_YEAR, course_code),
+		'learner_city_counts': map_queries.learner_city_counts(THIS_YEAR, course_code),
+		# Ratings
+		'all_ratings': ratings.all_ratings(),
+		# Comments
+		'general_comments': comments.general,
+		'technical_comments': comments.technical,
+		'language_comments': comments.language,
+		'performance_comments': comments.performance,
+		# Categorical and yes/no questions
+		'reason_to_participate': comments.reason,
+		'technical_issues': comments.technical_bool,
+		'languages_available': comments.language_bool,
+		'tools_used': comments.gccampus_bool,
+		'prepared_by': comments.preparation
+	}
+	return render_template('/course-page/main.html', pass_dict=pass_dict)
 
 
 # Temporary solution: Run queries for all course codes, store in dict, export to pickle
--- catalogue_app/db.py
+++ catalogue_app/db.py
@@ -1,17 +1,39 @@
+import copy
 import os
 from flask import g
 import mysql.connector
+from catalogue_app import memo_dict
 from catalogue_app.config import Config
 
 
 def query_mysql(query, args=None, dict_=False):
-	"""Execute query on connection stored in g."""
-	cnx = get_db(local=Config.LOCAL_DB)
-	cursor = cnx.cursor(dictionary=dict_)
-	cursor.execute(query, args)
-	results = cursor.fetchall()
-	cursor.close()
-	return results
+	"""Retrieve query if memoized else execute query on connection
+	stored in g.
+	"""
+	# Check if query already cached in memo_dict
+	query_pkey = _query_pkey(query, args)
+	if query_pkey in memo_dict:
+		print('Using memoized values for: {0}'.format(query_pkey))
+		# Return a deepcopy to avoid mutating and affecting future queries
+		return copy.deepcopy(memo_dict[query_pkey])
+	else:
+		print('Running query for the first time.')
+		cnx = get_db(local=Config.LOCAL_DB)
+		cursor = cnx.cursor(dictionary=dict_)
+		cursor.execute(query, args)
+		results = cursor.fetchall()
+		cursor.close()
+		# Save query in memo_dict for future use
+		memo_dict[query_pkey] = results
+		return results
+
+
+def _query_pkey(query, args):
+	"""Append 'query' and 'args' into a string for use as a primary key
+	to represent the query. No risk of SQL injection as memo_dict will
+	simply store memo_dict['malicious_query'] = None.
+	"""
+	return query + '.' + str(args)
 
 
 def get_db(local):
