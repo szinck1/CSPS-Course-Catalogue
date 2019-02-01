b5ee133e4d17022bb3407a6ea28ee10c091d50ca
deepklim <38829219+deepklim@users.noreply.github.com>
2019-02-01 12:53:17 -0500

    Update SQL schema to include FR

--- catalogue_app/course_routes/queries/general_queries.py
+++ catalogue_app/course_routes/queries/general_queries.py
@@ -23,8 +23,19 @@ def course_title(lang, fiscal_year, course_code):
 	return as_string(course_title, error_msg=False)
 
 
-def course_info(course_code):
-	query = "SELECT * FROM product_info WHERE course_code = %s LIMIT 1;"
+def course_info(lang, course_code):
+	if lang == 'fr':
+		fields = ['course_description_fr', 'business_type_fr', 'provider_fr',
+		'displayed_on_gccampus_fr', 'duration', 'main_topic_fr', 'business_line_fr',
+		'required_training_fr', 'communities_fr', 'point_of_contact', 'director',
+		'program_manager', 'project_lead']
+	else:
+		fields = ['course_description_en', 'business_type_en', 'provider_en',
+		'displayed_on_gccampus_en', 'duration', 'main_topic_en', 'business_line_en',
+		'required_training_en', 'communities_en', 'point_of_contact', 'director',
+		'program_manager', 'project_lead']
+	
+	query = "SELECT {0} FROM product_info WHERE course_code = %s LIMIT 1;".format(str(fields).replace('[', '').replace(']', '').replace("'", ''))
 	results = query_mysql(query, (course_code,), dict_=True)
 	# Format keys for displaying on page
 	results_processed = {_clean_key(key): val for (key, val) in results[0].items()}
@@ -32,8 +43,10 @@ def course_info(course_code):
 
 
 def _clean_key(key):
+	# First remove endings _en and _fr
+	# Then remove other underscores + junk
 	key = key.title()
-	replace_dict = {'Of': 'of', 'On': 'on', 'Gccampus': 'GCcampus', '_': ' '}
+	replace_dict = {'_En': '', '_Fr': '', '_': ' ', 'Of': 'of', 'On': 'on', 'Gccampus': 'GCcampus'}
 	for old, new in replace_dict.items():
 		key = key.replace(old, new)
 	return key
