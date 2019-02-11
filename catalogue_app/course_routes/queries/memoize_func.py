41fbc522cd09c52092970a1c6a5215f0ee3e0503
deepklim <38829219+deepklim@users.noreply.github.com>
2019-02-11 15:24:31 -0500

    Remove - no longer used

--- catalogue_app/course_routes/queries/memoize_func.py
+++
@@ -1,9 +0,0 @@
-from catalogue_app.config import Config
-from catalogue_app.course_routes.queries import
-
-LAST_YEAR = Config.LAST_YEAR
-THIS_YEAR = Config.THIS_YEAR
-
-
-def get_vals(lang, course_code):
-	return True
