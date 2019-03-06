de8b4c24d65b467b49cb37b1ae05655fec5a47e7
deepklim <38829219+deepklim@users.noreply.github.com>
2019-03-05 23:46:02 -0500

    Outline route for download raw data button

--- catalogue_app/download_routes/routes.py
+++ catalogue_app/download_routes/routes.py
@@ -1,9 +1,8 @@
-import csv
 import datetime
-from io import StringIO
 from flask import Blueprint, make_response
+from flask_babel import gettext
 from catalogue_app import auth
-# from catalogue_app.download_routes.queries import download_queries
+from catalogue_app.download_routes.queries import download_queries
 
 # Instantiate blueprint
 downloads = Blueprint('downloads', __name__)
@@ -12,44 +11,17 @@ downloads = Blueprint('downloads', __name__)
 @downloads.route('/download-general')
 @auth.login_required
 def download_general():
-	
-	
-	csvList = [
-		[1, 2, 3],
-		[4, 5, 6]
-	]
-	
-	si = StringIO()
-	cw = csv.writer(si)
-	cw.writerows(csvList)
-	
-	output = make_response(si.getvalue())
-	output.headers["Content-Disposition"] = "attachment; filename=export.csv"
-	output.headers["Content-type"] = "text/csv"
+	# Query raw data
+	raw_data = download_queries.general_tab('S202')
+	output = make_response(raw_data)
+	# Add headers with filename and type
+	filename = gettext('General Tab')
+	timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	output.headers['Content-Disposition'] = 'attachment; filename={0} {1}.csv'.format(filename, timestamp)
+	output.headers['Content-type'] = 'text/csv'
 	return output
-	
-	
-	
-	
-	
-	
-	
-	#timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-#	return send_file(si.getvalue(),
-#					 as_attachment=True,
-#					 attachment_filename='thicc {0}.txt'.format(timestamp),
-#					 mimetype='text/csv',
-#					 # Prevent caching
-#					 cache_timeout=-1)
-
-					 
-					 
-					 
-					 
-					 
-# Need to read through all docs, ensure doing properly
-# Ensure headers like attachment and meme type
-	# use curl to check if send_file options take care of headers
 
 
-# Push tonight? ;)
+# Ensure validate user input, only create CSV for safe course codes
+	# Simply produce CSV with BILINGUAL error message if junk is passed (test this)
+# Push to Azure
