4405c2e94b6e6fbf46d82a62b3a9af9c532da9d7
deepklim <38829219+deepklim@users.noreply.github.com>
2019-03-06 13:51:06 -0500

    Generalize code for use in all tabs

--- catalogue_app/download_routes/routes.py
+++ catalogue_app/download_routes/routes.py
@@ -15,27 +15,61 @@ downloads = Blueprint('downloads', __name__)
 def download_general():
 	# Get course code from query string and validate
 	course_code = request.args.get('course_code', False)
-	validate = _validate_course_code(course_code)
-	
-	# If course code successfully validated, query its raw
-	# data, else return error message
-	if validate:
-		raw_data = download_queries.general_tab(course_code)
-		output = make_response(raw_data)
-	else:
-		error_msg = gettext('Course Not Found')
-		output = make_response(error_msg)
-	
-	# Add headers with filename and MIME type
+	# Point to query used for this tab
+	query_func = download_queries.general_tab
+	# Set filename
 	filename = gettext('General Tab')
+	# Run query and prepare file
+	validate = _validate_course_code(course_code)
+	raw_data = _run_query(validate, query_func, course_code)
+	response = _create_response(raw_data, filename)
+	return response
+
+
+@downloads.route('/download-dashboard')
+@auth.login_required
+def download_dashboard():
+	# Get course code from query string and validate
+	course_code = request.args.get('course_code', False)
+	# Point to query used for this tab
+	query_func = download_queries.dashboard_tab
+	# Set filename
+	filename = gettext('Dashboard Tab')
+	# Run query and prepare file
+	validate = _validate_course_code(course_code)
+	raw_data = _run_query(validate, query_func, course_code)
+	response = _create_response(raw_data, filename)
+	return response
+
+
+
+
+
+
+
+def _validate_course_code(course_code):
+	"""Ensure course code exists prior to proceeding."""
+	# Inputs escaped in MySQL
+	course_title = utils.validate_course_code('en', Config.THIS_YEAR, course_code)
+	return True if course_title else False
+
+
+def _run_query(validate, query_func, course_code):
+	"""If course code successfully validated, query its raw
+	data, else return error message.
+	"""
+	if validate:
+		raw_data = query_func(course_code)
+	else:
+		raw_data = gettext('Course Not Found')
+	return raw_data
+
+
+def _create_response(raw_data, filename):
+	"""Prepare file for download by browser."""
+	output = make_response(raw_data)
 	timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 	# 'attachment' to ensure downloads rather than opened in browser
 	output.headers['Content-Disposition'] = 'attachment; filename="{0} {1}.csv"'.format(filename, timestamp)
 	output.headers['Content-Type'] = 'text/csv; charset=utf-8'
 	return output
-
-
-def _validate_course_code(course_code):
-	"""Ensure course code exists prior to proceeding."""
-	course_title = utils.validate_course_code('en', Config.THIS_YEAR, course_code)
-	return True if course_title else False
