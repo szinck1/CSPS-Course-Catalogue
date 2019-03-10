7c9b7dca84e56a3482eaf859a535734a801bedd7
deepklim <38829219+deepklim@users.noreply.github.com>
2019-03-10 18:00:21 -0400

    Continue rough outline

--- catalogue_app/api_routes/queries/comment_clustering_queries.py
+++ catalogue_app/api_routes/queries/comment_clustering_queries.py
@@ -1,10 +1,14 @@
 from flask import jsonify
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
-from catalogue_app.db import query_mysql
+from catalogue_app.course_routes.queries.comment_queries import Comments
 
 def lda():
-	corpus = ['mars loves food and cream',
+	
+	# Query comments
+	corpus = [comment[0] for comment in Comments('en', 'A313').load().general]
+	
+	corpus2 = ['mars loves food and cream',
 			  'mars loves science and tech',
 			  'cream is the food for mars',
 			  'mars likes tech but also science']
@@ -13,32 +17,55 @@ def lda():
 	# Stopwords, lemmatize, etc.
 		# pass
 	# Vectorize
-	vectorizer = CountVectorizer()
+	vectorizer = CountVectorizer(stop_words='english')
 	X = vectorizer.fit_transform(corpus)
 	
 	
-	print(sorted([(val, key) for key, val in vectorizer.vocabulary_.items()]))
+	#print(sorted([(val, key) for key, val in vectorizer.vocabulary_.items()]))
 	
 	# Instantiate model
-	TOPICS = 2
+	TOPICS = 3
 	lda = LatentDirichletAllocation(n_components=TOPICS,
 									doc_topic_prior=1/TOPICS,
 									topic_word_prior=1/TOPICS,
 									learning_method='batch', # 'online' for large
-									max_iter=10,
+									max_iter=50,
 									n_jobs=1,
 									verbose=0,
 									random_state=1) # Use constant random seed for dev
 	X_trans = lda.fit_transform(X)
 	
+	
+	
+	
+	def print_top_words(model, feature_names, n_top_words):
+		my_list = []
+		for topic_idx, topic in enumerate(model.components_):
+			message = "Topic #%d: " % topic_idx
+			message += " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
+			my_list.append(message)
+		return my_list
+	
+	
+	
+	return print_top_words(lda, vectorizer.get_feature_names(), 3)
+	
+	
+	
+	
+	
+	
+	
+	
 	# Return highest prob for each sample
-	return X_trans.argmax(axis=1)
+	#return X_trans.argmax(axis=1)
 	
 	# Same as built-in json.dumps but creates response, adds MIME type
 	#return jsonify(response)
 	
 	
 	# TODO
+	# French??
 	# Try NMF
 	# Automatically choose k
 	# Trying finding a single word to name clusters via wordnet
