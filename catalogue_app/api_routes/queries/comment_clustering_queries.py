6734d5c2f11c5593e361fdad1aef31dd8cf3d338
deepklim <38829219+deepklim@users.noreply.github.com>
2019-03-10 22:13:46 -0400

    Query DB for corpus

--- catalogue_app/api_routes/queries/comment_clustering_queries.py
+++ catalogue_app/api_routes/queries/comment_clustering_queries.py
@@ -1,71 +1,63 @@
-from flask import jsonify
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 from catalogue_app.course_routes.queries.comment_queries import Comments
 
-def lda():
-	
+def get_clusters(course_code):
+	"""Query a course's comments from DB and run LDA (unsupervised) algo to cluster
+	comments by subject matter.
+	"""
 	# Query comments
-	corpus = [comment[0] for comment in Comments('en', 'A313').load().general]
+	corpus = [comment[0] for comment in Comments('en', course_code).load().general]
 	
-	corpus2 = ['mars loves food and cream',
-			  'mars loves science and tech',
-			  'cream is the food for mars',
-			  'mars likes tech but also science']
+	# Pre-processing
 	# Lowercase
 	corpus = [elem.lower() for elem in corpus]
-	# Stopwords, lemmatize, etc.
-		# pass
+	
+	# Stopwords (only EN), lemmatize (only if sample has all ASCII chars)
+		# Idea: a 'French' subject usually appears, is useful
+			# Consequently, don't remove FR stopwords to encourage this formation
+	
 	# Vectorize
 	vectorizer = CountVectorizer(stop_words='english')
 	X = vectorizer.fit_transform(corpus)
 	
-	
-	#print(sorted([(val, key) for key, val in vectorizer.vocabulary_.items()]))
-	
-	# Instantiate model
+	# Instantiate and train model
 	TOPICS = 3
+	WORDS_PER_TOPIC = 3
 	lda = LatentDirichletAllocation(n_components=TOPICS,
 									doc_topic_prior=1/TOPICS,
 									topic_word_prior=1/TOPICS,
-									learning_method='batch', # 'online' for large
+									learning_method='batch',
 									max_iter=50,
 									n_jobs=1,
-									verbose=0,
-									random_state=1) # Use constant random seed for dev
+									verbose=False)
 	X_trans = lda.fit_transform(X)
 	
-	
-	
-	
-	def print_top_words(model, feature_names, n_top_words):
-		my_list = []
-		for topic_idx, topic in enumerate(model.components_):
-			message = "Topic #%d: " % topic_idx
-			message += " ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
-			my_list.append(message)
-		return my_list
-	
-	
-	
-	return print_top_words(lda, vectorizer.get_feature_names(), 3)
-	
-	
-	
-	
-	
-	
-	
-	
-	# Return highest prob for each sample
-	#return X_trans.argmax(axis=1)
-	
-	# Same as built-in json.dumps but creates response, adds MIME type
-	#return jsonify(response)
-	
-	
-	# TODO
-	# French??
-	# Try NMF
-	# Automatically choose k
-	# Trying finding a single word to name clusters via wordnet
+	return _get_clusters(lda, vectorizer.get_feature_names(), WORDS_PER_TOPIC)
+
+
+def _get_clusters(model, feature_names, n_top_words):
+	""""""
+	clusters = {}
+	for ctr, topic in enumerate(model.components_):
+		key = 'Cluster {0}'.format(ctr)
+		val = ' '.join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
+		clusters[key] = val
+	return clusters
+
+
+
+# X_trans.argmax(axis=1) to get best label for each sample
+
+# TODO
+# French??
+# Validation / error if passed junk
+# Try NMF
+# Wrap it all in Sklearn pipeline
+# Automatically choose k
+# Trying finding a single word to name clusters via wordnet
+
+
+
+
+
