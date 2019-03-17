eb06c6d0ebda83efa517e9e77fd0c5c72871540f
deepklim <38829219+deepklim@users.noreply.github.com>
2019-03-16 23:58:16 -0400

    Disable forcing JSON to be returned in ASCII

--- catalogue_app/config.py
+++ catalogue_app/config.py
@@ -6,6 +6,8 @@ class Config:
 	LAST_YEAR = '2017_18'
 	THIS_YEAR = '2018_19'
 	BABEL_DEFAULT_LOCALE = 'en'
+	# Ensure FR properly displayed when returning JSON
+	JSON_AS_ASCII = False
 	# Load strings from environ vars to avoid storing in plaintext
 	BASIC_AUTH_USERNAME = os.environ.get('BASIC_AUTH_USERNAME')
 	BASIC_AUTH_PASSWORD = os.environ.get('BASIC_AUTH_PASSWORD')
